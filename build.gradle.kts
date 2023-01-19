741132d31211a83f646f998dc852b065fc93efb9
RyoNaganuma0505 <ryo.naganuma.career@gmail.com>
2023-01-19 10:57:06 +0900

    バージョン整理

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ val kotlin_version: String by project
 val logback_version: String by project
 
 plugins {
-    kotlin("jvm") version "1.8.0"
+    kotlin("jvm") version "1.6.10"
     id("io.ktor.plugin") version "2.2.2"
 }
 
@@ -22,8 +22,11 @@ repositories {
 
 dependencies {
     implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:$kotlin_version")
     implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+    implementation("com.expediagroup:graphql-kotlin-server:7.0.0-alpha.0")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:$kotlin_version")
 }
