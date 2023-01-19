8f0d756e2c3c8aa1c6bc48a05387c44b2d8bf993
RyoNaganuma0505 <ryo.naganuma.career@gmail.com>
2023-01-19 12:25:23 +0900

    引数付きQuery

--- src/main/kotlin/com/example/usecases/GetUser.kt
+++ src/main/kotlin/com/example/usecases/GetUser.kt
@@ -6,4 +6,7 @@ import com.example.dto.UserData
 object GetUser {
     fun execute(): UserData =
         UserData("Taro", AddressData("Tokyo", "Shibuya"))
+
+    fun execute(id: String): UserData =
+        UserData("Taro$id", AddressData(" Tokyo ", " Shibuya "))
 }
